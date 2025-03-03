2029054c11a3c831fbf13a35dae384292d5b8c9e
Rodger Milla Dev <rodgermilladev@gmail.com>
2025-03-04 00:43:27 +0300

    New Update

--- JS/index.js
+++ JS/index.js
@@ -5,7 +5,7 @@ function toHome(){
     document.getElementById("drawerTitle").innerText='Home'
     document.getElementById("actDrawerCart").style.right='-101%'
     document.getElementById("actDrawerProfile").style.right='-101%'
-
+    document.getElementById("actDrawerProduct").style.right='-101%'
     document.getElementById("actDrawerShop").style.right='0%'
 
 }
@@ -17,6 +17,7 @@ function toProducts(){
     document.getElementById("shopProducts").style.height='78vh'
     document.getElementById("drawerTitle").innerText='All Products'
     document.getElementById("actDrawerCart").style.right='-101%'
+    document.getElementById("actDrawerProduct").style.right='-101%'
     document.getElementById("actDrawerProfile").style.right='-101%'
     document.getElementById("actDrawerShop").style.right='0%'
 
@@ -28,6 +29,7 @@ function toOffers(){
     document.getElementById("shopProducts").style.top='10vh'
     document.getElementById("shopProducts").style.height='78vh'
     document.getElementById("drawerTitle").innerText='On Offer'
+    document.getElementById("actDrawerProduct").style.right='-101%'
     document.getElementById("actDrawerCart").style.right='-101%'
     document.getElementById("actDrawerProfile").style.right='-101%'
     document.getElementById("actDrawerShop").style.right='0%'
@@ -36,8 +38,8 @@ function toOffers(){
 
 function toCart(){
     document.getElementById("drawerTitle").innerText='My Cart'
-
     document.getElementById("actDrawerProfile").style.right='-101%'
+    document.getElementById("actDrawerProduct").style.right='-101%'
     document.getElementById("actDrawerShop").style.right='-101%'
     document.getElementById("actDrawerCart").style.right='0%'
 }
@@ -45,6 +47,20 @@ function toCart(){
 function toProfile(){
     document.getElementById("drawerTitle").innerText='Profile Settings'
     document.getElementById("actDrawerShop").style.right='-101%'
+    document.getElementById("actDrawerProduct").style.right='-101%'
     document.getElementById("actDrawerCart").style.right='-101%'
     document.getElementById("actDrawerProfile").style.right='0'
+
+}
+
+function toProduct(){
+    document.getElementById("fidiShopOffer").style.top='0vh'
+    document.getElementById("catnSearchCont").style.top='35vh'
+    document.getElementById("shopProducts").style.top='45vh'
+    document.getElementById("drawerTitle").innerText='Home'
+    document.getElementById("actDrawerCart").style.right='-101%'
+    document.getElementById("actDrawerProfile").style.right='-101%'
+
+    document.getElementById("actDrawerShop").style.right='-101%'
+    document.getElementById("actDrawerProduct").style.right='0%'
 }
