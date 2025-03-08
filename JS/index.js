e4088415998b11c3f13ad9b203d345b0a95350e7
Rodger Milla Dev <rodgermilladev@gmail.com>
2025-03-08 18:23:51 +0300

    New Update

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,6 @@
+
 function toHome(){
+
     document.getElementById("fidiShopOffer").style.top='0vh'
     document.getElementById("catnSearchCont").style.top='35vh'
     document.getElementById("shopProducts").style.top='45vh'
@@ -6,11 +8,14 @@ function toHome(){
     document.getElementById("actDrawerCart").style.right='-101%'
     document.getElementById("actDrawerProfile").style.right='-101%'
     document.getElementById("actDrawerProduct").style.right='-101%'
+    document.getElementById("checkoutPage").style.right='-101%'
+    document.getElementById("actDrawerSuccessCheck").style.right='-101%'
     document.getElementById("actDrawerShop").style.right='0%'
 
 }
 
 function toProducts(){
+   
     document.getElementById("fidiShopOffer").style.top='-100vh'
     document.getElementById("catnSearchCont").style.top='0vh'
     document.getElementById("shopProducts").style.top='10vh'
@@ -19,11 +24,14 @@ function toProducts(){
     document.getElementById("actDrawerCart").style.right='-101%'
     document.getElementById("actDrawerProduct").style.right='-101%'
     document.getElementById("actDrawerProfile").style.right='-101%'
+    document.getElementById("checkoutPage").style.right='-101%'
+    document.getElementById("actDrawerSuccessCheck").style.right='-101%'
     document.getElementById("actDrawerShop").style.right='0%'
 
 }
 
 function toOffers(){
+
     document.getElementById("fidiShopOffer").style.top='-100vh'
     document.getElementById("catnSearchCont").style.top='0vh'
     document.getElementById("shopProducts").style.top='10vh'
@@ -32,35 +40,75 @@ function toOffers(){
     document.getElementById("actDrawerProduct").style.right='-101%'
     document.getElementById("actDrawerCart").style.right='-101%'
     document.getElementById("actDrawerProfile").style.right='-101%'
+    document.getElementById("checkoutPage").style.right='-101%'
+    document.getElementById("actDrawerSuccessCheck").style.right='-101%'
     document.getElementById("actDrawerShop").style.right='0%'
 
 }
 
 function toCart(){
+
     document.getElementById("drawerTitle").innerText='My Cart'
     document.getElementById("actDrawerProfile").style.right='-101%'
     document.getElementById("actDrawerProduct").style.right='-101%'
     document.getElementById("actDrawerShop").style.right='-101%'
+    document.getElementById("checkoutPage").style.right='-101%'
+    document.getElementById("actDrawerSuccessCheck").style.right='-101%'
     document.getElementById("actDrawerCart").style.right='0%'
 }
 
 function toProfile(){
+
     document.getElementById("drawerTitle").innerText='Profile Settings'
     document.getElementById("actDrawerShop").style.right='-101%'
     document.getElementById("actDrawerProduct").style.right='-101%'
     document.getElementById("actDrawerCart").style.right='-101%'
+    document.getElementById("checkoutPage").style.right='-101%'
+    document.getElementById("actDrawerSuccessCheck").style.right='-101%'
     document.getElementById("actDrawerProfile").style.right='0'
 
 }
 
 function toProduct(){
+
     document.getElementById("fidiShopOffer").style.top='0vh'
     document.getElementById("catnSearchCont").style.top='35vh'
     document.getElementById("shopProducts").style.top='45vh'
-    document.getElementById("drawerTitle").innerText='Home'
+    document.getElementById("drawerTitle").innerText='Product'
     document.getElementById("actDrawerCart").style.right='-101%'
     document.getElementById("actDrawerProfile").style.right='-101%'
-
     document.getElementById("actDrawerShop").style.right='-101%'
+    document.getElementById("checkoutPage").style.right='-101%'
+    document.getElementById("actDrawerSuccessCheck").style.right='-101%'
     document.getElementById("actDrawerProduct").style.right='0%'
 }
+
+function toCheckout(){
+   
+    document.getElementById("fidiShopOffer").style.top='0vh'
+    document.getElementById("catnSearchCont").style.top='35vh'
+    document.getElementById("shopProducts").style.top='45vh'
+    document.getElementById("drawerTitle").innerText='Product'
+    document.getElementById("actDrawerCart").style.right='-101%'
+    document.getElementById("actDrawerProfile").style.right='-101%'
+    document.getElementById("actDrawerShop").style.right='-101%'
+    document.getElementById("actDrawerProduct").style.right='-101%'
+    document.getElementById("actDrawerSuccessCheck").style.right='-101%'
+    document.getElementById("checkoutPage").style.right='0%'
+}
+
+function toSuccess(){
+   
+    document.getElementById("fidiShopOffer").style.top='0vh'
+    document.getElementById("catnSearchCont").style.top='35vh'
+    document.getElementById("shopProducts").style.top='45vh'
+    document.getElementById("drawerTitle").innerText='Product'
+    document.getElementById("actDrawerCart").style.right='-101%'
+    document.getElementById("actDrawerProfile").style.right='-101%'
+    document.getElementById("actDrawerShop").style.right='-101%'
+    document.getElementById("actDrawerProduct").style.right='-101%'
+    document.getElementById("checkoutPage").style.right='-101%'
+    document.getElementById("actDrawerSuccessCheck").style.right='0%'
+
+}
+
